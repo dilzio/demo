a4f65fe88a6ba77eb79b50145cb2f6e8e6af3ea5
dilzio <mtclarkson@yahoo.com>
2024-12-04 11:57:37 -0600

    add go

---
+++ go/go.mod
@@ -0,0 +1,10 @@
+module demo
+
+go 1.23.2
+
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
---
+++ go/main.go
@@ -0,0 +1,10 @@
+package main
+
+import	_ "github.com/go-chi/chi/v5"
+import  _ "github.com/go-chi/chi/v5/middleware"
+import	_ "github.com/sirupsen/logrus"
+import	_ "net/http"
+
+func main() {
+// do nothing
+}
